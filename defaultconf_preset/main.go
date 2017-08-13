100c5e269687828cb15e55283b1e104f0f1e21c0
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-08-13 17:32:35 +0200

    Move defaultconf_1 to defaultconf_preset

---
+++ defaultconf_preset/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/Horgix/packer-builder-defaultconf/defaultconf_preset/defaultconf"
+	"github.com/hashicorp/packer/packer/plugin"
+	"log"
+)
+
+func main() {
+	log.Println("Starting...")
+	server, err := plugin.Server()
+	if err != nil {
+		panic(err)
+	}
+
+	server.RegisterBuilder(defaultconf.NewBuilder())
+	server.Serve()
+}
